50d80ae173c99cb7f1e0273c602d36f6b93a0224
MaxLiv <gigofrimin@gmail.com>
2017-10-05 17:04:04 +0300

    2-nd commit

--- app/src/main/java/com/example/max/myapplication/MainActivity.java
+++ app/src/main/java/com/example/max/myapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.max.myapplication;
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     TextView playerTextView;
     List<String> playersList = new ArrayList<>();
+    ImageButton resetButton;
 
     int count = 0;
     int size;
@@ -31,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         playerTextView = (TextView) findViewById(R.id.playerTextView);
         layout = (ConstraintLayout) findViewById(R.id.mainView);
+        resetButton = (ImageButton) findViewById(R.id.resetButton);
 
         collectPlayers();
     }
@@ -46,9 +53,50 @@ public class MainActivity extends AppCompatActivity {
         Random random = new Random();
         int colors[] = { Color.BLUE, Color.GREEN, Color.YELLOW, Color.RED};
         int pos = random.nextInt(colors.length);
-        layout.setBackgroundColor(colors[pos]);
+
+        changeViewColor(layout, resetButton,getViewBackgroundColor(layout),colors[pos]);
+
         setPlayer();
         changeColor(colors[pos]);
+    }
+
+    private int getViewBackgroundColor(View view) {
+        int color = Color.TRANSPARENT;
+        Drawable background = layout.getBackground();
+        if (background instanceof ColorDrawable)
+            color = ((ColorDrawable) view.getBackground()).getColor();
+        return color;
+
+    }
+
+    private void changeViewColor(final View view1, final View view2, final int initialColor, final int finalColor) {
+        // Load initial and final colors.
+
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // Use animation position to blend colors.
+                float position = animation.getAnimatedFraction();
+                int blended = blendColors(initialColor, finalColor, position);
+
+                // Apply blended color to the view.
+                view1.setBackgroundColor(blended);
+                view2.setBackgroundColor(blended);
+            }
+        });
+
+        anim.setDuration(600).start();
+    }
+
+    private int blendColors(int from, int to, float ratio) {
+        final float inverseRatio = 1f - ratio;
+
+        final float r = Color.red(to) * ratio + Color.red(from) * inverseRatio;
+        final float g = Color.green(to) * ratio + Color.green(from) * inverseRatio;
+        final float b = Color.blue(to) * ratio + Color.blue(from) * inverseRatio;
+
+        return Color.rgb((int) r, (int) g, (int) b);
     }
 
     private void setPlayer() {
@@ -61,29 +109,63 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void changeViewTextColor(final TextView view1, final TextView view2, final int initialColor, final int finalColor) {
+        // Load initial and final colors.
+
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 1);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // Use animation position to blend colors.
+                float position = animation.getAnimatedFraction();
+                int blended = blendColors(initialColor, finalColor, position);
+
+                // Apply blended color to the view.
+                view1.setTextColor(blended);
+                view2.setTextColor(blended);
+            }
+        });
+
+        anim.setDuration(600).start();
+    }
+
     void changeColor(int color){
         switch (color){
             case Color.BLUE: {
 //                setTheme(R.style.AppThemeBlue);
+                setWhiteColorView();
                 chooseBodyPart("синее");
             }
                 break;
             case Color.GREEN: {
 //                getTheme().applyStyle(R.style.AppThemeGreen, true);
+                setBlackColorView();
                 chooseBodyPart("зелёное");
             }
                 break;
             case Color.RED: {
 //                this.setTheme(R.style.AppThemeRed);
+                setWhiteColorView();
                 chooseBodyPart("красное");
             }
                 break;
             case Color.YELLOW: {
 //                setTheme(R.style.AppThemeYellow);
+                setBlackColorView();
                 chooseBodyPart("желтое");
             }
                 break;
         }
+    }
+
+    private void setWhiteColorView(){
+        changeViewTextColor(textView, playerTextView, textView.getCurrentTextColor(), Color.WHITE);
+//        textView.setTextColor(Color.WHITE);
+//        playerTextView.setTextColor(Color.WHITE);
+    }
+
+    private void setBlackColorView(){
+        changeViewTextColor(textView, playerTextView, textView.getCurrentTextColor(), Color.BLACK);
     }
 
     void chooseBodyPart(String color){
@@ -103,4 +185,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void reset(View view) {
+        Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+        startActivity(intent);
+    }
 }
